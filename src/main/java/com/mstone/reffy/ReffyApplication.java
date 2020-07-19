3288ee50e8853501895afe1c3c9e4e7518385ea1
Malcolm Stone <m.stone@kew.org>
2020-07-19 10:15:22 +0100

    add layout dialect dependency

--- src/main/java/com/mstone/reffy/ReffyApplication.java
+++ src/main/java/com/mstone/reffy/ReffyApplication.java
@@ -2,8 +2,13 @@ package com.mstone.reffy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.templateresolver.ITemplateResolver;
+
+import nz.net.ultraq.thymeleaf.LayoutDialect;
 
 @SpringBootApplication
 @EnableAsync
@@ -11,5 +16,13 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class ReffyApplication {
   public static void main(String[] args) {
     SpringApplication.run(ReffyApplication.class, args);
+  }
+
+  @Bean
+  SpringTemplateEngine templateEngine(ITemplateResolver templateResolver) {
+    var templateEngine = new SpringTemplateEngine();
+    templateEngine.addTemplateResolver(templateResolver);
+    templateEngine.addDialect(new LayoutDialect());
+    return templateEngine;
   }
 }
