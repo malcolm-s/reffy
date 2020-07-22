06a851d5e6829cbc62f49979f0ba77c6f0e46560
Malcolm Stone <m.stone@kew.org>
2020-07-22 11:03:44 +0100

    initial security config

--- src/main/java/com/mstone/reffy/ReffyApplication.java
+++ src/main/java/com/mstone/reffy/ReffyApplication.java
@@ -1,10 +1,20 @@
 package com.mstone.reffy;
 
+import com.mstone.reffy.auth.ReffyUserDetailsService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.templateresolver.ITemplateResolver;
 
@@ -24,5 +34,22 @@ public class ReffyApplication {
     templateEngine.addTemplateResolver(templateResolver);
     templateEngine.addDialect(new LayoutDialect());
     return templateEngine;
+  }
+}
+
+@Configuration
+@EnableWebMvc
+class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+  @Autowired
+  private ReffyUserDetailsService reffyUserDetailsService;
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.authorizeRequests(auth -> auth.antMatchers("/referendums/{id}/vote").authenticated().anyRequest().permitAll());
+  }
+
+  @Bean
+  public UserDetailsService userDetailsService() {
+    return reffyUserDetailsService;
   }
 }
---
+++ src/main/java/com/mstone/reffy/auth/ReffyUserDetailsService.java
@@ -0,0 +1,37 @@
+package com.mstone.reffy.auth;
+
+import java.util.Arrays;
+import java.util.List;
+
+import com.mstone.reffy.user.UserRepository;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@Slf4j
+public class ReffyUserDetailsService implements UserDetailsService {
+  private final UserRepository userRepository;
+
+  public ReffyUserDetailsService(UserRepository userRepository) {
+    this.userRepository = userRepository;
+  }
+
+  @Override
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    log.info("loadUserByUsername: {}", username);
+    return userRepository.findByEmail(username).map(u -> new User(u.getEmail(), u.getPassword(), getAuthorities()))
+        .orElseThrow(() -> new UsernameNotFoundException("No user found with email: " + username));
+  }
+
+  private List<GrantedAuthority> getAuthorities() {
+    return Arrays.asList(new SimpleGrantedAuthority("USER"));
+  }
+}
