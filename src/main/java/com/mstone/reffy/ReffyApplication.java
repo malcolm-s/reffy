faf3911b5b53006ff20e8f4fd60b6ce66737a7fe
Malcolm Stone <m.stone@kew.org>
2020-07-23 10:18:41 +0100

    move web security config to new file

--- src/main/java/com/mstone/reffy/ReffyApplication.java
+++ src/main/java/com/mstone/reffy/ReffyApplication.java
@@ -1,21 +1,10 @@
 package com.mstone.reffy;
 
-import com.mstone.reffy.auth.ReffyUserDetailsService;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.security.config.annotation.ObjectPostProcessor;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.templateresolver.ITemplateResolver;
 
@@ -35,30 +24,5 @@ public class ReffyApplication {
     templateEngine.addTemplateResolver(templateResolver);
     templateEngine.addDialect(new LayoutDialect());
     return templateEngine;
-  }
-}
-
-@Configuration
-@EnableWebSecurity
-class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-  @Autowired
-  private ReffyUserDetailsService reffyUserDetailsService;
-
-  @Override
-  protected void configure(HttpSecurity http) throws Exception {
-    http.authorizeRequests(auth -> auth.antMatchers("/referendums/{id}/vote").authenticated().anyRequest().permitAll())
-        .formLogin(formLogin -> formLogin.loginPage("/login").usernameParameter("email"))
-        .logout(logout -> logout.logoutUrl("/logout").logoutSuccessUrl("/login?loggedOut"))
-        .csrf(csrf -> csrf.disable());
-  }
-
-  @Override
-  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-    auth.userDetailsService(reffyUserDetailsService);
-  }
-
-  @Bean
-  public PasswordEncoder passwordEncoder() {
-    return PasswordEncoderFactories.createDelegatingPasswordEncoder();
   }
 }
