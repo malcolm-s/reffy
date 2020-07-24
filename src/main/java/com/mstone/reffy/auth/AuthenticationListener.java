eadfad6daed196b2ba25740b65586c7fd6660299
Malcolm Stone <m.stone@kew.org>
2020-07-24 11:09:05 +0100

    merge authentication listeners

--- src/main/java/com/mstone/reffy/auth/AuthenticationListener.java
+++ src/main/java/com/mstone/reffy/auth/AuthenticationListener.java
@@ -1,8 +1,9 @@
 package com.mstone.reffy.auth;
 
-import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.EventListener;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
+import org.springframework.security.authentication.event.LogoutSuccessEvent;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
@@ -10,11 +11,18 @@ import lombok.extern.slf4j.Slf4j;
 
 @Component
 @Slf4j
-public class LoginListener implements ApplicationListener<AuthenticationSuccessEvent> {
-  @Override
-  public void onApplicationEvent(AuthenticationSuccessEvent event) {
+public class AuthenticationListener {
+  @EventListener
+  public void onLogin(AuthenticationSuccessEvent event) {
     var token = (UsernamePasswordAuthenticationToken) event.getSource();
     var user = (UserDetails) token.getPrincipal();
     log.info("login successful: {}", user.getUsername());
+  }
+
+  @EventListener
+  public void onLogout(LogoutSuccessEvent event) {
+    var token = (UsernamePasswordAuthenticationToken) event.getSource();
+    var user = (UserDetails) token.getPrincipal();
+    log.info("logout successful: {}", user.getUsername());
   }
 }
