c47411d02f1b3f96a0c55e688171c4787b711a01
Malcolm Stone <m.stone@kew.org>
2020-07-24 10:56:55 +0100

    rework csv gov id service to read file only once

--- src/main/java/com/mstone/reffy/govid/CsvFileGovIdService.java
+++ src/main/java/com/mstone/reffy/govid/CsvFileGovIdService.java
@@ -3,8 +3,14 @@ package com.mstone.reffy.govid;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Scope;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
 
@@ -12,24 +18,32 @@ import lombok.extern.slf4j.Slf4j;
 
 @Component
 @Slf4j
+@Scope("application")
 public class CsvFileGovIdService implements GovIdService {
   @Value("classpath:data/govids.csv")
   private Resource csvFile;
+  private Set<String> govIds;
 
   @Override
   public boolean isValidGovId(String govId) {
+    return govIds.contains(govId);
+  }
+
+  @PostConstruct
+  private void init() {
+    log.info("running postconstruct");
+    govIds = ConcurrentHashMap.newKeySet();
     try (var reader = new BufferedReader(new InputStreamReader(csvFile.getInputStream()))) {
       String line;
       do {
         line = reader.readLine();
-        if (line.equals(govId)) {
-          return true;
+        if (line != null) {
+          govIds.add(line);
         }
       } while (line != null);
+      log.info("govids after reading file: {}", govIds);
     } catch (IOException ex) {
       log.error("error validating gov id", ex);
-      return false;
     }
-    return false;
   }
 }
