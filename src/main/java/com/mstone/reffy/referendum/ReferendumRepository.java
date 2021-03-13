05ddcd410377e221dd7f56453d9bb4701fffd5f0
Malcolm Stone <m.stone@kew.org>
2021-03-13 21:24:58 +0000

    format sql in ReferendumRepository

--- src/main/java/com/mstone/reffy/referendum/ReferendumRepository.java
+++ src/main/java/com/mstone/reffy/referendum/ReferendumRepository.java
@@ -16,12 +16,12 @@ public interface ReferendumRepository extends JpaRepository<Referendum, Integer>
 
   @Transactional
   @Modifying
-  @Query("UPDATE Referendum r set r.votesForCount = r.votesForCount + 1 WHERE r.id = :id")
+  @Query("UPDATE Referendum r SET r.votesForCount = r.votesForCount + 1 WHERE r.id = :id")
   void incrementVotesForCountById(@Param("id") Integer id);
 
   @Transactional
   @Modifying
-  @Query("UPDATE Referendum r set r.votesAgainstCount = r.votesAgainstCount + 1 WHERE r.id = :id")
+  @Query("UPDATE Referendum r SET r.votesAgainstCount = r.votesAgainstCount + 1 WHERE r.id = :id")
   void incrementVotesAgainstCountById(@Param("id") Integer id);
 
   Page<Referendum> findAllWithCategoriesBy(Pageable page);
